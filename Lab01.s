ad04fcf1efe05fa143ae3f6464aef132b5c927f7
MichaelTMcClellan <90366746+MichaelTMcClellan@users.noreply.github.com>
2021-09-08 22:52:39 -0500

    Add files via upload

--- Lab01.s
+++ Lab01.s
@@ -16,3 +16,37 @@
 @    gcc -o Lab01 Lab01.o
 @    ./Lab01 ;echo $?
 @    gdb --args ./Lab01
+
+	.global main
+	
+main:
+	MOV R7, #0x04
+	MOV R0, #0x01
+	MOV R2, #0x0C
+	LDR   r1, =welcomestring
+	SVC 0
+	
+	LDR  r0, =stringarray1
+	BL printf
+	
+	MOV r7, #0x01
+	SVC 0
+	
+	
+writeloop:
+	CMP R0, #10
+	BEQ writedone
+	
+	ADD R0, R0, #1
+	
+.data
+.balign 4
+welcomestring: .asciz "Now printing three arrays\n" @Length 0x1A
+.balign 4
+stringarray1: .asciz "Now printing array one\n" @Length 0x17
+.balign 4
+stringarray2: .asciz "Now printing array two\n" @Length 0x17
+.balign 4
+stringarray3: .asciz "Now printing array three\n" @Length 0x19
+
+.global printf
