4af395102b07a94781e906a822b9c926df091fd7
MichaelTMcClellan <90366746+MichaelTMcClellan@users.noreply.github.com>
2021-09-10 00:03:42 -0500

    Add files via upload

--- Lab01.s
+++ Lab01.s
@@ -43,17 +43,45 @@ addloop:
 	
 	LDR  r0, =stringarray1 		@ load string1 into r0
 	BL printf @ Call printf
+
+	LDR r4, =array1				@ load address of array to be printed to r4
+	PUSH {r4}					@ push r4 on to the stack
+	BL printarraysub			
 	
 	LDR  r0, =stringarray2 		@ load string2 into r0
 	BL printf @ Call printf
 	
-	LDR  r0, =stringsumarray 		@ load string3 into r0
+	LDR r4, =array2				@ load address of array to be printed to r4
+	PUSH {r4}					@ push r4 on to the stack
+	BL printarraysub			
+	
+	LDR  r0, =stringsumarray 	@ load string3 into r0
 	BL printf @ Call printf
 	
-@ Exit program/return to OS.	
-
+	LDR r4, =sumarray			@ load address of array to be printed to r4
+	PUSH {r4}					@ push r4 on to the stack
+	BL printarraysub	
+	
+	@ Exit program/return to OS.	
 	MOV r7, #0x01
 	SVC 0
+	
+printarraysub:
+	POP {r4}
+	PUSH {r14}
+	MOV r8, #0					@ initialize r8 for loop counter
+printloop:
+	LDR r0, =loopstring			@ load %d into r1
+	LDR r1, [r4] #4				@ load value to be printed into r1, increment array pointer
+	BL printf					@ print decimal of array element
+	CMP r8, #10					@ compare r8 to 10
+	BNE printloop	
+	LDR r10, =linebreak			@ load linebreak into r0
+	BL printf					@ print linebreak after list
+	POP  {r14}
+	B r14
+	
+	
 	
 .data
 .balign 4
@@ -61,6 +89,8 @@ welcomestring: .asciz "Welcome to Lab 01\n"
 stringarray1: .asciz "Now printing array one\n"
 stringarray2: .asciz "Now printing array two\n"
 stringsumarray: .asciz "Now printing summed array\n"
+loopstring: .asciz "%d, "
+linebreak: .asciz "\n"
 
 array1: .word 32, 64, -9, 42, 234, 55, 92, 0, -23, -55
 array2: .word -32, 11, 6, -2222, -55, 1, 4, 0, 522, 7
@@ -69,7 +99,7 @@ sumarray: .word 99, 99, 99 ,99 ,99 ,99, 99, 99, 99, 99
 .global scanf
 .global printf
 @	r0 - starting address of string to be printed
-@	r1 - If the string contains an output, i.e., r1 must contain the value to be printed.
+@	r1 - If the string contains an output, r1 must contain the value to be printed.
 @	When the call returns registers: r0, r1, r2, r3 and r12 are changed.
 
 @	end of code and end of file
